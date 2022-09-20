17f268c1f13167927c0c4b8bc13f2152ac6d28b0
FiRe <levih@n11.dev>
2022-09-19 19:59:45 -0500

    Potential fix for update.sh
    Signed-off-by: FiRe <levih@n11.dev>

--- update.sh
+++ update.sh
@@ -1,22 +1,22 @@
 sed -i -e 's/3000/1337/g' Source/main.cpp
 make
-if [ ! -f /web/levihicks.dev/ ]; then
+if [ ! -e /web/levihicks.dev/ ]; then
     sudo mkdir /web/levihicks.dev/
 fi
-if [ -f /web/levihicks.dev/www/ ]; then
+if [ -e /web/levihicks.dev/www/ ]; then
     sudo rm -r /web/levihicks.dev/www/
 fi
-if [ -f /web/levihicks.dev/favicon/ ]; then
+if [ -e /web/levihicks.dev/favicon/ ]; then
     sudo rm -r /web/levihicks.dev/favicon
 fi
-if [ -f /web/levihicks.dev/Link ]; then
+if [ -e /web/levihicks.dev/Link ]; then
     sudo rm /web/levihicks.dev/Link
 fi
 sudo cp Website /web/levihicks.dev/Link
 sudo cp -R www/ /web/levihicks.dev/www/
 sudo cp -R favicon/ /web/levihicks.dev/favicon/
 sudo chown -R web:web /web/levihicks.dev/
-if [ ! -f /etc/systemd/system/levihicks.dev.service ]; then
+if [ ! -e /etc/systemd/system/levihicks.dev.service ]; then
     sudo cp levihicks.dev.service /etc/systemd/system/levihicks.dev.service
     sudo systemctl daemon-reload
     sudo systemctl enable levihicks.dev.service
